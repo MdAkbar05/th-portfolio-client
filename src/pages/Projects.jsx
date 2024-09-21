1b12b0e46b50371d8d22ac673870deb6182307bc
MdAkbar05 <samratakbar667466@gmail.com>
2024-09-22 00:15:09 +0600

    updated

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -20,7 +20,7 @@ const Projects = () => {
     dispatch(fetchProjects());
   }, []);
   return (
-    <Container fluid className="project-section">
+    <Container fluid className="project-section" style={{ height: "90vh" }}>
       <Particle />
       <Container>
         <h1 className="project-heading">
@@ -32,7 +32,7 @@ const Projects = () => {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           {isLoading ? (
             <h2>Projects Loading .....</h2>
-          ) : (
+          ) : projects.length > 0 ? (
             projects.map((project) => {
               return (
                 <Col key={project._id} md={4} className="project-card">
@@ -46,58 +46,11 @@ const Projects = () => {
                 </Col>
               );
             })
+          ) : (
+            <h3 className="text-danger">
+              Problem with Server to fetching, Maybe your server is disconnected
+            </h3>
           )}
-          {/* 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={project}
-              isBlog={false}
-              title="Social Media"
-              description="This platform features a responsive interface built with Reactjs and CSS, allowing users to easily manage their profiles and posts through CRUD activities. The option to follow other users and interact with their posts (like or dislike) is also available, with suggestions for new users to follow. Users can only view posts from their followed users and can only engage in chat with those they follow. The backend is powered by Nodejs and Expressjs server, while MongoDB is utilized as the database."
-              ghLink="https://github.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={kickstart}
-              isBlog={false}
-              title="E‑Commerce Website"
-              description="This ecommerce website utilizes a REST API built with Nodejs and Expressjs, allowing for easy CRUD operations on user profiles, products and admin panel. The user interface is designed for efficient product management and includes a complete purchase process. The admin panel, accessible for CRUD on users and products, includes a chart for statistics display. Stripe API is integrated to handle customer payments. The website uses MongoDB and Firebase as the database."
-              ghLink=""
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={pg}
-              isBlog={false}
-              title="Blog Website"
-              description="This blogging application features a RESTful API server built with Nodejs and Expressjs. The data is retrieved from the API server and displayed on a user-friendly interface designed using React, Redux, and styled-components for a seamless experience."
-              ghLink=""
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={krypto}
-              isBlog={false}
-              title="Project Krypto"
-              description="Project Krypto is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It allows users to interact with their Metamask wallet and send Ethereum."
-              //ghLink=""
-              demoLink=""
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={kickstart}
-              isBlog={false}
-              title="Solidity Kickstart"
-              description="Solidity Kickstart is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It includes all the functionality of a kickstart platform, enabling users to interact with their Metamask wallet, transfer Ethereum and participate in kickstart campaigns."
-              ghLink="https://github.com/"
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
